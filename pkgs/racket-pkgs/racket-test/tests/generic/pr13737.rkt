87eff67e96989443028a50acefcad81fbc5e5be0
Carl Eastlund <cce@racket-lang.org>
2013-06-02 20:41:09 -0400

    Updated tests for new syntax.

--- pkgs/racket-pkgs/racket-test/tests/generic/pr13737.rkt
+++ pkgs/racket-pkgs/racket-test/tests/generic/pr13737.rkt
@@ -28,6 +28,7 @@
   (define-generics (foo gen:foo prop:foo foo?
                         #:defined-table dummy
                         #:defaults ([number? (define (meth foo #:kw kw) kw)])
+                        #:fallback ()
                         #:prop-defined-already? foo-accessor
                         #:define-contract #f)
     (meth foo #:kw kw))
