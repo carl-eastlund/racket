69b4c05fdf8f2d7ba7a1e29ad2aeb215f8277c1e
Carl Eastlund <cce@racket-lang.org>
2013-06-05 05:11:24 -0400

    Added syntax validation for free-identifier=?, bound-identifier=?, and identifier-binding.

--- collects/racket/private/base.rkt
+++ collects/racket/private/base.rkt
@@ -14,71 +14,116 @@
              "generic-interfaces.rkt"
              (for-syntax "stxcase-scheme.rkt"))
 
-  (define unique (gensym))
-
-  (define (syntax-local-value/immediate/validate
-            id
-            [fail #f]
-            [ctx #f])
-    (validate-id! id)
-    (define (fail/unique) (values #f unique))
+  (define (step-identifier id ctx proc-exn proc-value proc-syntax proc-rename)
     (define-values (value target)
-      (syntax-local-value/immediate id fail/unique ctx))
+      (with-handlers ([exn:fail? (lambda (e) (values #f 'error))])
+        (syntax-local-value/immediate id (lambda () (values #f 'value)) ctx)))
     (cond
-      [(eq? target unique)
-       (cond
-         [fail (fail)]
-         [else (raise-syntax-error 'syntax-local-value/immediate
-                 (format "not defined as syntax\n  identifier: ~e" id)
-                 id)])]
       [(identifier? target)
-       (validate-id! target)
        (unless (or (syntax-property target 'not-free-identifier=?)
-                   (free-identifier=? id target))
+                 (free-identifier=? id target))
          (eprintf "~a:\n  ~e\n  ~e\n"
            "rename transformer but not free-identifier=?"
            id
            target))
-       (values value target)]
-      [else
-       (values value target)]))
+       (proc-rename value target)]
+      [(eq? target 'value) (proc-value)]
+      [(eq? target 'error) (proc-exn)]
+      [else (proc-syntax value)]))
 
-  (define (syntax-local-value/validate id0 [fail #f] [ctx #f])
-    (define (fail/immediate) (values #f unique))
-    (let loop ([id id0])
-      (define-values (value target)
-        (syntax-local-value/immediate/validate id fail/immediate ctx))
+  (define (chase-identifier id ctx proc-exn proc-value proc-syntax)
+    (let loop ([id id])
+      (step-identifier id ctx
+        (lambda () (proc-exn))
+        (lambda () (proc-value id))
+        (lambda (value) (proc-syntax id value))
+        (lambda (value target) (loop target)))))
+
+  (define (syntax-local-value/immediate/validate id [fail #f] [ctx #f])
+    (step-identifier id ctx
+      (lambda ()
+        (syntax-local-value/immediate id fail ctx))
+      (lambda ()
+        (cond
+          [fail (fail)]
+          [else (raise-syntax-error 'syntax-local-value/immediate
+                  (format "not defined as syntax\n  identifier: ~e" id)
+                  id)]))
+      (lambda (value) (values value #f))
+      (lambda (value target) (values value target))))
+
+  (define (syntax-local-value/validate id [fail #f] [ctx #f])
+    (chase-identifier id ctx
+      (lambda ()
+        (syntax-local-value id fail ctx))
+      (lambda (id)
+        (cond
+          [fail (fail)]
+          [else (raise-syntax-error 'syntax-local-value/immediate
+                  (format "not defined as syntax\n  identifier: ~e" id)
+                  id)]))
+      (lambda (id value) value)))
+
+  (define (chase-ok? phase)
+    (and (syntax-transforming?)
+      (equal? phase (syntax-local-phase-level))))
+
+  (define (identifier-binding/validate id [phase (syntax-local-phase-level)])
+    (cond
+      [(chase-ok? phase)
+       (chase-identifier id #f
+         (lambda () (identifier-binding id phase))
+         (lambda (id) (identifier-binding id phase))
+         (lambda (id value) (identifier-binding id phase)))]
+      [else (identifier-binding id phase)]))
+
+  (define (identifier=?/validate a b phase)
+    (define free=?
       (cond
-        [(eq? target unique)
-         (cond
-           [fail (fail)]
-           [else (raise-syntax-error 'syntax-local-value
-                   (format "not defined as syntax\n  identifier: ~e" id0)
-                   id0)])]
-        [(identifier? target) (loop target)]
-        [else value])))
-
-  (define (validate-id! id)
-    (define id2 (syntax-local-introduce id))
-    (check-id! id)
-    (check-id! id2)
-    (check-ids! id id2)
-    (hash-set! id-table id id2))
-
-  (define (check-id! id)
-    (for ([(k v) (in-hash id-table)])
-      (check-ids! id k)
-      (check-ids! id v)))
-
-  (define (check-ids! a b)
-    (when (bound-identifier=? a b)
-      (unless (free-identifier=? a b)
-        (eprintf "~a:\n  ~e\n  ~e\n"
+        [(chase-ok? phase)
+         (free-identifier=?
+           (chase-identifier a #f
+             (lambda () a)
+             (lambda (id) id)
+             (lambda (id value) id))
+           (chase-identifier b #f
+             (lambda () b)
+             (lambda (id) id)
+             (lambda (id value) id))
+           phase)]
+        [else (free-identifier=? a b phase)]))
+    (define bound=?
+      (bound-identifier=? a b phase))
+    (when bound=?
+      (unless free=?
+        (eprintf "~a at phase ~a:\n  ~e\n  ~e\n"
           "bound-identifier=? but not free-identifier=?"
+          phase
           a
-          b))))
+          b)))
+    (values free=? bound=?))
 
-  (define id-table (make-hasheq))
+  (define (bound-identifier=?/validate a b [phase (syntax-local-phase-level)])
+    (define-values (free=? bound=?)
+      (identifier=?/validate a b phase))
+    bound=?)
+
+  (define (free-identifier=?/validate a b
+            [a-phase (syntax-local-phase-level)]
+            [b-phase a-phase])
+    (cond
+      [(equal? a-phase b-phase)
+       (define-values (free=? bound=?)
+         (identifier=?/validate a b a-phase))
+       free=?]
+      [else (free-identifier=? a b a-phase b-phase)]))
+
+  (define (free-transformer-identifier=?/validate a b)
+    (free-identifier=?/validate a b (add1 (syntax-local-phase-level))))
+  (define (free-template-identifier=?/validate a b)
+    (free-identifier=?/validate a b (sub1 (syntax-local-phase-level))))
+  (define (free-label-identifier=?/validate a b)
+    (free-identifier=?/validate a b #f))
 
   (#%provide (all-from-except "pre-base.rkt"
                               open-input-file
@@ -92,10 +137,29 @@
                               regexp-replace*
                               new-apply-proc
                               syntax-local-value
-                              syntax-local-value/immediate)
-             (rename syntax-local-value/validate syntax-local-value)
+                              syntax-local-value/immediate
+                              identifier-binding
+                              free-identifier=?
+                              free-transformer-identifier=?
+                              free-template-identifier=?
+                              free-label-identifier=?
+                              bound-identifier=?)
+             (rename syntax-local-value/validate
+                     syntax-local-value)
              (rename syntax-local-value/immediate/validate
                      syntax-local-value/immediate)
+             (rename identifier-binding/validate
+                     identifier-binding)
+             (rename free-identifier=?/validate
+                     free-identifier=?)
+             (rename free-transformer-identifier=?/validate
+                     free-transformer-identifier=?)
+             (rename free-template-identifier=?/validate
+                     free-template-identifier=?)
+             (rename free-label-identifier=?/validate
+                     free-label-identifier=?)
+             (rename bound-identifier=?/validate
+                     bound-identifier=?)
              struct
              (all-from "hash.rkt")
              (all-from "list.rkt")
