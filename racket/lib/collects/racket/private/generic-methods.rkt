933ad08d3f5d6fe6e17eede2a5a79858c2f319fb
Carl Eastlund <cce@racket-lang.org>
2013-06-05 16:40:57 -0400

    Restored implementation of define/generic.

--- racket/lib/collects/racket/private/generic-methods.rkt
+++ racket/lib/collects/racket/private/generic-methods.rkt
@@ -77,87 +77,31 @@
            (raise-syntax-error 'define/generic "expected an identifier" #'bind))
          (unless (identifier? #'ref)
            (raise-syntax-error 'define/generic "expected an identifier" #'ref))
-         (define i1 syntax-local-introduce)
-         (define d1 (syntax-local-make-delta-introducer gen-id))
-         (define s1 syntax-local-get-shadower)
-         (define (i stxs) (map i1 stxs))
-         (define (d stxs) (map d1 stxs))
-         (define (s stxs) (map s1 stxs))
-         (define ms (generic-info-methods gen-val))
-         (with-syntax ([(m ...) ms]
-                       [(im ...) (i ms)]
-                       [(dm ...) (d ms)]
-                       [(sm ...) (s ms)]
-                       [(idm ...) (d (i ms))]
-                       [(ism ...) (s (i ms))]
-                       [(dim ...) (i (d ms))]
-                       [(dsm ...) (s (d ms))]
-                       [(sim ...) (i (s ms))]
-                       [(sdm ...) (d (s ms))]
-                       [(idsm ...) (s (d (i ms)))]
-                       [(isdm ...) (d (s (i ms)))]
-                       [(dism ...) (s (i (d ms)))]
-                       [(dsim ...) (i (s (d ms)))]
-                       [(sidm ...) (d (i (s ms)))]
-                       [(sdim ...) (i (d (s ms)))])
-           #'(define/generic-from bind ref
-               ["m" m ...]
-               ["im" im ...]
-               ["dm" dm ...]
-               ["sm" sm ...]
-               ["idm" idm ...]
-               ["ism" ism ...]
-               ["dim" dim ...]
-               ["dsm" dsm ...]
-               ["sim" sim ...]
-               ["sdm" sdm ...]
-               ["idsm" idsm ...]
-               ["isdm" isdm ...]
-               ["dism" dism ...]
-               ["dsim" dsim ...]
-               ["sidm" sidm ...]
-               ["sdim" sdim ...])))]))
-
-  (define-syntax (define/generic-from stx)
-    (define source-ids
-      (let loop ([stx (stx-cdr stx)] [ids '()])
-        (syntax-case stx ()
-          [(a . b) (loop #'a (loop #'b ids))]
-          [i (identifier? #'i) (cons #'i ids)]
-          [_ ids])))
-    (define all-ids
-      (append source-ids (map syntax-local-introduce source-ids)))
-    (define bound-pairs
-      (let loop ([xs all-ids] [ys all-ids] [pairs '()])
-        (cond
-          [(null? xs) (reverse pairs)]
-          [(null? ys) (loop (cdr xs) (cdr xs) pairs)]
-          [else
-           (define x (car xs))
-           (define y (car xs))
-           (if (and (bound-identifier=? x y)
-                    (not (free-identifier=? x y)))
-               (loop xs (cdr ys) (cons (list x y) pairs))
-               (loop xs (cdr ys) pairs))])))
-    (define free-pairs
-      (let loop1 ([xs all-ids] [pairs '()])
-        (cond
-          [(null? xs) (reverse pairs)]
-          [else
-           (let loop2 ([x (car xs)] [pairs pairs])
-             (define-values (value target)
-               (syntax-local-value/immediate x
-                 (lambda () (values #f #f))))
+         (define methods
+           (map syntax-local-get-shadower
+                (map (syntax-local-make-delta-introducer gen-id)
+                     (generic-info-methods gen-val))))
+         (define matches
+           (let loop ([methods methods])
              (cond
-               [(identifier? target)
-                (if (free-identifier=? x target)
-                    (loop2 target pairs)
-                    (loop2 target (cons (list x target) pairs)))]
-               [else (loop1 (cdr xs) pairs)]))])))
-    (with-syntax ([([a b] ...) bound-pairs]
-                  [([c d] ...) free-pairs])
-      #'(bad-ids (#:bound-but-not-free [a b] ...)
-                 (#:rename-but-not-free [c d] ...))))
-
-  (define-syntax (bad-ids stx)
-    (raise-syntax-error 'bad-ids "stop here!" stx)))
+               [(null? methods) '()]
+               [(free-identifier=? (car methods) #'ref)
+                (cons (car methods) (loop (cdr methods)))]
+               [else
+                (loop (cdr methods))])))
+         (unless (pair? matches)
+           (raise-syntax-error 'define/generic
+                               (format "~.s is not a method of ~.s"
+                                       (syntax-e #'ref)
+                                       (syntax-e gen-id))
+                               stx
+                               #'ref))
+         (when (pair? (cdr matches))
+           (raise-syntax-error 'define/generic
+                               (format "multiple methods match ~.s: ~.s"
+                                       (syntax-e #'ref)
+                                       (map syntax-e matches))
+                               stx
+                               #'ref))
+         (with-syntax ([method (car matches)])
+           #'(define bind method)))])))
