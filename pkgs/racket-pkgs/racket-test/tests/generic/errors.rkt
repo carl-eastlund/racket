7d67826707ecdafcd1149ad38748cdeb06bb24f5
Carl Eastlund <cce@racket-lang.org>
2013-06-05 20:43:16 -0400

    Made #:methods error message tests more consistent.

--- pkgs/racket-pkgs/racket-test/tests/generic/errors.rkt
+++ pkgs/racket-pkgs/racket-test/tests/generic/errors.rkt
@@ -14,7 +14,7 @@
   (check-exn #rx"not a name for a generics group"
              (lambda () (convert-compile-time-error
                          (struct foo () #:methods bad ()))))
-  (check-exn #rx"method definition has an incorrect arity"
+  (check-exn #rx"generic method definition has an incorrect arity"
              (lambda () (convert-compile-time-error
                          (let ()
                           (define-generics foobar
@@ -23,7 +23,7 @@
                                   #:methods gen:foobar
                                   [(define (foo) 0)])
                           'ignore))))
-  (check-exn #rx"method definition has an incorrect arity"
+  (check-exn #rx"generic method definition has an incorrect arity"
              (lambda () (convert-compile-time-error
                          (let ()
                           (define-generics foobar
